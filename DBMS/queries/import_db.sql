82a72e13e9ad9defcf9ad524519014a4b6c774fc
srmono <bvsrao91@gmail.com>
2023-05-15 09:11:45 +0530

    MySQL db updated

---
+++ DBMS/queries/import_db.sql
@@ -0,0 +1,8 @@
+-- Step 1: Download DB
+-- Step 2: Connect to database 
+--     > mysql -u root -p
+--     Enter password: ********
+-- Step 3: Import using following command 
+--     source path\mysqlsampledatabase.sql
+-- Step 4: Verify
+--     show databases;
