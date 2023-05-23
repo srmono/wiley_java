03b6c7d359802068290ae5d472cf040e497c925f
srmono <bvsrao91@gmail.com>
2023-05-23 16:59:49 +0530

    JDBC transactions updated

--- jdbcapp/src/main/java/com/wiley/jdbcapp/App.java
+++ jdbcapp/src/main/java/com/wiley/jdbcapp/App.java
@@ -6,61 +6,101 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.sql.Date;
 
 public class App {
-	public static void main(String[] args) throws SQLException {
-		
+	public static void addCandidate(String firstName, String lastName, Date dob, String email, String phone,
+			int[] skills) throws SQLException {
+
 		Connection conn = null;
 		PreparedStatement ps = null;
 		PreparedStatement psSkills = null;
 		ResultSet rs = null;
-		
+
 		try {
 			DBConnection dbcon = new DBConnection();
 			conn = dbcon.createDBConnection();
-			
-			/*JDBC Transactions*/
-			
-			//Disable the autocommit
+
+			/* JDBC Transactions */
+
+			// Disable the autocommit
 			conn.setAutoCommit(false);
-			
-			//Here is your transaction
-			
-			//If everything is ok, commit the transaction
-			conn.commit();
-			
-			//insert the candidate
+
+			// Here is your transaction
+			// insert the candidate
 			String sqlInsert = "INSERT INTO candidates(first_name, last_name, dob, phone, email) VALUES(?,?,?,?,?)";
+
+			ps = conn.prepareStatement(sqlInsert, Statement.RETURN_GENERATED_KEYS);
 			
-			ps = conn.prepareStatement(sqlInsert);
+			ps.setString(1, firstName);
+			ps.setString(2, lastName);
+			ps.setDate(3, dob);
+			ps.setString(4, phone);
+			ps.setString(5, email);
 			
+			int rowAffected = ps.executeUpdate();
 			
+			rs = ps.getGeneratedKeys();
+			System.out.println("Generated keys" + rs);
 			
-//			//create statement object
-//			Statement stmt = conn.createStatement();
-//			
-//			//MySQL query
-//			String sql = "SELECT first_name, last_name, email FROM candidates ";
-//			
-//			ResultSet rs = stmt.executeQuery(sql);
-//			
-//			while(rs.next()) {
-//				System.out.println(
-//						rs.getString("first_name") + "\t" + rs.getString("last_name") + "\t" + rs.getString("email")
-//						);
-//			}
-//			
-//			try {
-//				rs.close();
-//				stmt.close();
-//			} catch (SQLException e) {
-//				System.out.println(e.getMessage());
-//			}
+			int candidateId = 0;
+			if(rs.next())
+				candidateId = rs.getInt(1);
+			System.out.println("Candidate id: " + candidateId);
 			
+			if(rowAffected == 1) {
+				String sqlSkillInsert = "INSERT INTO candidate_skills(candidate_id, skill_id) VALUES(?,?)";
+				
+				psSkills = conn.prepareStatement(sqlSkillInsert);
+				
+				for(int skillId: skills) {
+					psSkills.setInt(1, candidateId);
+					psSkills.setInt(2, skillId);
+					
+					psSkills.executeUpdate();
+				}
+				conn.commit();
+			} else {
+				System.out.println("roll back done");
+				conn.rollback();
+			}
+
+			// If everything is ok, commit the transaction
+			//conn.commit();
+
 		} catch (SQLException e) {
-			//in case of exception, rollback the transaction
-			conn.rollback();
-			e.printStackTrace();
+			try {
+				if(conn != null)
+					conn.rollback();
+			} catch (SQLException e2) {
+				System.out.println(e2.getMessage());
+			}
+		} finally {
+			try {
+				if(rs !=null) rs.close();
+				if(ps !=null) ps.close();
+				if(psSkills !=null) psSkills.close();
+				if(conn !=null) conn.close();
+				
+			} catch (SQLException e2) {
+				System.out.println(e2.getMessage());
+			}
 		}
 	}
+
+	public static void main(String[] args) throws SQLException {
+		int[] skills = {1,2,3};
+		
+		addCandidate("Venkat", "B", Date.valueOf("1990-02-04"), "bvsrao91@gmail.com", "9035351966", skills);
+	}
 }
+
+
+
+
+
+
+
+
+
+
