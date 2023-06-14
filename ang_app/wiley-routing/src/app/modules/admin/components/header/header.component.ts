1cd74e4898d2ba7049012256953adda18714a45c
srmono <bvsrao91@gmail.com>
2023-06-14 08:54:39 +0530

    Angular updated

--- ang_app/wiley-routing/src/app/modules/admin/components/header/header.component.ts
+++ ang_app/wiley-routing/src/app/modules/admin/components/header/header.component.ts
@@ -1,14 +1,17 @@
-import { AuthService } from './../../../../services/auth/auth.service';
+//import { AuthService } from './../../../../services/auth/auth.service';
 import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
-  constructor(private auth: AuthService) {}
+  constructor(
+    private auth: AuthService
+  ) {}
 
   ngOnInit(): void {}
   logout(): void {
